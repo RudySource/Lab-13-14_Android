71d21cf98d7bcab2e39495645399bc3415d7aecc
Rud3us <rodionpylnev@yandex.ru>
2026-04-13 22:35:07 +0300

    Step 4: Add words data and game logic

--- app/src/main/java/com/rudy/unscramblegame/ui_model/GameViewModel.kt
+++ app/src/main/java/com/rudy/unscramblegame/ui_model/GameViewModel.kt
@@ -1,7 +1,8 @@
 package com.rudy.unscramblegame.ui_model
 
-import androidx.lifecycle.ViewModel
 import com.rudy.unscramblegame.data.GameUiState
+import androidx.lifecycle.ViewModel
+import com.rudy.unscramblegame.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -12,7 +13,30 @@ class GameViewModel : ViewModel() {
     init {
         resetGame()
     }
+    private  lateinit var currentWord: String
+    private  var usedWords: MutableSet<String> = mutableSetOf()
 
-    fun resetGame(){
+    private  fun shuffleCurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+        tempWord.shuffle()
+        while (String(tempWord) == word) {
+            tempWord.shuffle()
+        }
+        return String(tempWord)
+    }
+
+    private fun pickRandomWordAndShuffle() : String {
+        currentWord = allWords.random()
+        while (usedWords.contains(currentWord)) {
+            currentWord = allWords.random()
+        }
+        usedWords.add(currentWord)
+        return shuffleCurrentWord((currentWord))
+    }
+    fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(
+            currentScrambleWord = pickRandomWordAndShuffle()
+        )
     }
 }
